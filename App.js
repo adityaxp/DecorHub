ea27c736472e19291f0320a298df0d6cc7145e0b
Aditya <adityabalsane10@gmail.com>
2024-01-31 13:05:55 +0530

    🎨 Added custom fonts

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    extrabold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    semibold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>DécorHub</Text>
+      <Text style={styles.textStyle}>DécorHub</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +38,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textStyle: {
+    fontFamily: "extrabold",
+    fontSize: 20,
   },
 });
