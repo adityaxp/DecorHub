2a47cd35387f2b17897f2fdaeb8cbb6fa1277f63
Aditya <adityabalsane10@gmail.com>
2024-01-31 15:05:45 +0530

    🏠 Home screen update

--- src/features/Home/screens/HomeScreen.jsx
+++ src/features/Home/screens/HomeScreen.jsx
@@ -1,15 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import { CustomSafeAreaView } from "../../../components/CustomSafeAreaView";
+import styles from "../styles/home.styles";
+import { Ionicons, Fontisto } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   return (
     <CustomSafeAreaView>
-      <Text>HomeScreen</Text>
+      <View style={styles.appBarWrapper}>
+        <View style={styles.appBar}>
+          <Ionicons name="location-outline" size={24} />
+          <Text style={styles.location}>Mumbai, India</Text>
+          <View style={{ alignItems: "flex-end" }}>
+            <View style={styles.cartCount}>
+              <Text style={styles.cartNumber}>0</Text>
+            </View>
+            <TouchableOpacity>
+              <Fontisto name="shopping-bag" size={24} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </CustomSafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default HomeScreen;
---
+++ src/features/Home/styles/home.styles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+import { COLORS, SIZES } from "../../../infrastructure/theme";
+
+const styles = StyleSheet.create({
+  appBarWrapper: {
+    marginHorizontal: 22,
+    marginTop: SIZES.small,
+  },
+  appBar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  location: {
+    fontFamily: "semibold",
+    fontSize: SIZES.medium,
+    color: COLORS.gray,
+  },
+  cartCount: {
+    position: "absolute",
+    bottom: 16,
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    alignItems: "center",
+    backgroundColor: "green",
+    justifyContent: "center",
+    zIndex: 999,
+  },
+  cartNumber: {
+    fontFamily: "regular",
+    fontWeight: "600",
+    fontSize: 10,
+    color: COLORS.lightWhite,
+  },
+  textStyle: {
+    fontFamily: "bold",
+    fontSize: 40,
+  },
+});
+
+export default styles;
