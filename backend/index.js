792fb5f2ce7cb1f0b9a263d41369b8e54daf8858
Aditya <adityabalsane10@gmail.com>
2024-02-08 19:26:07 +0530

    📁Backend setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
